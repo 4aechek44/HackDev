69631bcfed6f3ba255ca77e182ef29214bcafead
4aechek44 <egorkanon04@gmail.com>
2026-04-27 20:47:59 +0500

    update quests

--- my-react-app/src/components/quests.jsx
+++ my-react-app/src/components/quests.jsx
@@ -1,16 +1,30 @@
 import './quests.css';
 
+// Quests block
+const quests = [
+  {
+  id: 1,
+  title: 'test',
+  quest: 'input "hack start" in console',
+  expected: 'hack start',
+  reward: { xp: 10, money: 100},
+  unlocked: true
+  }
+]
+
 function Quests() {
   return (
-    <div className="quests-container">
-      <h2>Quests</h2>
-      <ul>
-        <li>quest 1</li>
-        <li>quest 2</li>
-        <li>quest 3</li>
-      </ul>
+    <div>
+    {quests.map(quest => (
+      <div key={quest.id} className="quests-container">
+        <h2>Quests</h2>
+        <h4>{quest.title}</h4>
+        <p className='quest'>{quest.quest}</p>
+        <p className='reward'>xp: {quest.reward.xp}, $: {quest.reward.money}</p>
+      </div>
+    ))}
     </div>
-  )
-};
+  );
+}
 
 export default Quests;
