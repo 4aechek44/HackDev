e703258d93fdb5647df0f2b648027abb81b9aae4
4aechek44 <egorkanon04@gmail.com>
2026-04-27 21:45:33 +0500

    delete navbar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,14 @@
-import Navbar from "./components/Navbar";
 import Quests from "./components/Quests";
 import Money from "./components/Money";
 import Level from "./components/Level";
 import CodeEditor from "./components/CodeEditor"
 import Hacker from './components/Hacker';
 import { GameProvider } from "./components/GameContext";
-import './App.css';
 
 function App() {
   return (
     <div>
     <GameProvider>
-      <Navbar />
       <Quests />
       <Money />
       <Level />
