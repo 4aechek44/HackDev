6f47e9f8045e07317523fb711623f737a4a53766
4aechek44 <egorkanon04@gmail.com>
2026-05-09 18:37:38 +0500

    nav level css update

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -12,6 +12,7 @@ function Navbar() {
   const nextLevelXp = getXpForLevel(level + 1) - currentLevelXp
 
   return (
+    // left side - logo and title
     <nav className="navbar">
       <h4 className="navbar-title">
         <img className="nav-logo" src="/favicon.svg" alt="Logo" />
@@ -34,16 +35,16 @@ function Navbar() {
           Settings
         </button>
       </div>
-
+       {/* right side - level, xp bar and avatar */}
       <div className='navbar-info'>
         <img className="nav-star" src="/star.png" alt="star" />
         <span className='nav-xp'>Level: </span>
         
-      <span>{level}</span>
+      <span className ='nav-level'>{level}</span>
         <div className="xp-bar">
-        
+          <div className="xp-fill" style={{ width: `${progress * 100}%` }} />
         </div>
-      <span>|{xp - currentLevelXp} / {nextLevelXp} XP</span>  
+      <span className='nav-xp-bar'>{xp - currentLevelXp} / {nextLevelXp} XP</span>  
         
         <button className="avatar-btn">
           <img className="nav-avatar" src="/avatar.png" alt="avatar" />
